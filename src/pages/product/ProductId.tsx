853f6644e81763d067329ae68fb85ba623eac11b
elmira1385 <elmirashokriyan64@gmail.com>
2026-07-05 21:55:02 +0430

    somethings were modified

--- src/pages/product/ProductId.tsx
+++ src/pages/product/ProductId.tsx
@@ -1,14 +1,127 @@
-import React from 'react'
-import { useParams } from 'react-router'
-import { useProducts } from '../../api'
+import React, { useState } from "react";
+import { useParams } from "react-router";
+import { useProducts } from "../../api";
 
 const ProductId = () => {
-   const{ProductId}= useParams()
-   const{data}=useProducts()
-   const item=data?.find((item)=>(item.id===Number(ProductId)))
-  return (
-    <div key={item?.id}>{item?.title}</div>
-  )
-}
+  const { ProductId } = useParams();
+  const { data } = useProducts();
+  const item = data?.find((item) => item.id === Number(ProductId));
+  const [mainImage, setMainImage] = useState("");
 
-export default ProductId
+  return (
+    <div className="flex flex-col gap-4 p-6">
+      <div className="  text-t-slate-700 text-sm ">{`Home / Products / ${item?.title.slice(0, 12)}`}</div>
+      <div className="flex flex-col gap-8">
+        <div className="flex flex-col gap-4">
+          <div className=" p-10 bg-slate-100 rounded-lg ">
+            <img
+              className="rounded-lg"
+              src={mainImage || item?.images[0]}
+              alt=""
+            />
+          </div>
+          <div className="flex sm:flex-col gap-3">
+            {item?.images.map((i, index) => (
+              <div
+                onClick={() => {
+                  setMainImage(i);
+                }}
+                key={index}
+                className="bg-slate-100 p-4 rounded-lg group cursor-pointer"
+              >
+                <img src={i} alt="" />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-col p-4 ">
+          <div className="flex flex-col justify-start items-start gap-2">
+            <h1 className="text-3xl font-semibold text-slate-800">
+              Smart speaker gray
+            </h1>
+            <div className="flex items-center ">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="14"
+                height="14"
+                viewBox="0 0 24 24"
+                fill="#00C950"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-star text-transparent mt-0.5"
+                aria-hidden="true"
+              >
+                <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"></path>
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="14"
+                height="14"
+                viewBox="0 0 24 24"
+                fill="#00C950"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-star text-transparent mt-0.5"
+                aria-hidden="true"
+              >
+                <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"></path>
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="14"
+                height="14"
+                viewBox="0 0 24 24"
+                fill="#00C950"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-star text-transparent mt-0.5"
+                aria-hidden="true"
+              >
+                <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"></path>
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="14"
+                height="14"
+                viewBox="0 0 24 24"
+                fill="#00C950"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-star text-transparent mt-0.5"
+                aria-hidden="true"
+              >
+                <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"></path>
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="14"
+                height="14"
+                viewBox="0 0 24 24"
+                fill="#D1D5DB"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-star text-transparent mt-0.5"
+                aria-hidden="true"
+              >
+                <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"></path>
+              </svg>
+              <p className="text-sm ml-3 text-slate-500">6 Reviews</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductId;
